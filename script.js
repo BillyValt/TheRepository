d9379e39a06b4ee138f794dcbc761e9187e435a9
BillyValt <123888056+BillyValt@users.noreply.github.com>
2023-12-02 23:13:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function pickPrize() {
     ` : `
       <div class="winner__incription">Подарок:</div>
         <div class="container__element">
-        <img class="container__img" id="img-id-0" src="images/randomPrize4.png">
+        <img class="container__img" id="img-id-0" src="images/randomPrize6.png">
       </div>
   ` ;
   }, 2100);
